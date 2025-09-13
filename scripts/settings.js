320a0657a421880bb0429df76f04703abab1dbe2
Emdi <markdhenn05@gmail.com>
2025-09-14 00:20:21 +0800

    a little changes

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,10 +1,28 @@
 const toggleMode = document.querySelector('.switch');
 const appContainer = document.querySelector('.container');
+const settings = document.querySelector('.settings');
+const selectTheme = document.querySelector('.select-theme');
+const appHeader   = document.querySelector('.app-header');
+const root = document.documentElement;
+const backToMenu = document.querySelector('.back-to-menu');
 
-let darkMode = false;
+backToMenu.addEventListener('click', () => {
+  settings.classList.add('close')
+})
+
+let darkMode = localStorage.getItem('darkMode') || false;
+switchMode()
 toggleMode.addEventListener('click', () => {
   darkMode = !darkMode;
+  localStorage.setItem('darkMode', darkMode)
+  switchMode();
+});
+
+function switchMode(){
+  !darkMode ? root.style.setProperty("--color-accent", "#262626") : root.style.setProperty("--color-accent", "#00FF80");
+  
   toggleMode.classList.toggle('dark', darkMode);
+  [settings, selectTheme].forEach(el => el.classList.toggle('light', !darkMode));
   document.body.classList.toggle('light', !darkMode);
-})
+}
 
