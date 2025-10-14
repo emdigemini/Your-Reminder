607111ac8eeabd16707fd377fc2eff0564d5a879
Emdi Gemini <yougotjinx@gmail.com>
2025-10-14 10:36:19 +0800

    stabilized controlGoal by preventing multiple bindings and using container queries instead of index mapping

--- scripts/settings.js
+++ scripts/settings.js
@@ -17,8 +17,14 @@ function switchMode(){
   // for background
   !darkMode ? root.style.setProperty("--color-background", "#fff8f0") : root.style.setProperty("--color-background", "#000");
   
+  // for empty state
+  !darkMode ? root.style.setProperty("--empty-state", "#262626B5") : root.style.setProperty("--empty-state", "#fff8f0");
+  
   // for text color
-  !darkMode ? root.style.setProperty("--color-accent", "#8c5e4f") : root.style.setProperty("--color-accent", "#00FF80");
+  !darkMode ? root.style.setProperty("--card-goal", "#fff8f0a2") : root.style.setProperty("--card-goal", "#262626");
+
+  // for card color
+
   
   toggleMode.classList.toggle('dark', darkMode);
   [settings, selectTheme].forEach(el => el.classList.toggle('light', !darkMode));
